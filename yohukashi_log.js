4a37f314aea497105227088069eef7d696bd55aa
BStrpg <120103302+BStrpg@users.noreply.github.com>
2022-12-23 00:04:44 +0900

    Add files via upload

--- yohukashi_log.js
+++ yohukashi_log.js
@@ -84,6 +84,30 @@ function playMc9() {maya_music(play9,mc9_tugunai);};
 // }
 // }, false );
 // }
+$('.pagetop')
+    .bind('touchstart', function(){
+        $(this).addClass('hover');
+    }).bind('touchend', function(){
+        $(this).removeClass('hover');
+});
+$('a.page_btn')
+    .bind('touchstart', function(){
+        $(this).addClass('hover');
+    }).bind('touchend', function(){
+        $(this).removeClass('hover');
+});
+$('a.page_btn_width')
+    .bind('touchstart', function(){
+        $(this).addClass('hover');
+    }).bind('touchend', function(){
+        $(this).removeClass('hover');
+});
+$('a.btn--red')
+    .bind('touchstart', function(){
+        $(this).addClass('hover');
+    }).bind('touchend', function(){
+        $(this).removeClass('hover');
+});
 
 // スクロールすると表示される
 $windowWidth = window.innerWidth;
